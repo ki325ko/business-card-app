7678c9313b4c2757bad111221ff1bfe330ea7499
ki325ko <ki325ko@gmail.com>
2026-04-20 16:56:40 +0900

    カード/テーブル切り替え表示機能を追加

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meishi-v2';
+const CACHE = 'meishi-v3';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   self.skipWaiting();
